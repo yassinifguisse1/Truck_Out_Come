54b2d3e50e3c3044bab0ec46f38f842eb4983f1f
yassinifguisse1 <36793818+yassinifguisse1@users.noreply.github.com>
2024-10-16 21:06:24 +0200

    added ui libraries

---
+++ components/ui/MeteorsSolution.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export const MeteorsSolution = ({
+  number,
+  className,
+}: {
+  number?: number;
+  className?: string;
+}) => {
+  const meteors = new Array(number || 30).fill(true);
+  return (
+    <>
+      {meteors.map((el, idx) => (
+        <span
+          key={"meteor" + idx}
+          className={cn(
+            "animate-meteor-effect absolute top-0 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]",
+            "before:content-[''] before:absolute before:top-0 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent",
+            className
+          )}
+          style={{
+            top: Math.floor(Math.random() * -200) + "px", 
+            left: Math.floor(Math.random() * (800 - -800) + -800) + "px",
+            animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+            animationDuration: Math.floor(Math.random() * (10 - 2) + 2) + "s",
+          }}
+        ></span>
+      ))}
+    </>
+  );
+};
