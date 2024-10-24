ebba616a64b720025cc582a8fe77f87a779da0b0
yassinifguisse1 <36793818+yassinifguisse1@users.noreply.github.com>
2024-10-24 16:08:33 +0200

    Cards done translating

--- app/_components/Different.tsx
+++ app/_components/Different.tsx
@@ -2,18 +2,23 @@
 import React from 'react'
 
 import { MeteorsDemo } from './MeteorsDemo'
+import { useTranslations } from 'next-intl';
 
-const cards =[
-  { title : "We Win When You Win" , description : "Think of us as your dedicated and innovative marketing partner, not just another vendor. We take your success personally and strive for lasting results."},
-  {title : "We're Here in Poland, Ready to Help" , description :"Not a call center. Not outsourced. We're real, local, and ready to sit down and discuss your needs face-to-face if that's what you want."},
-  {title : "Focused on What Matters" , description : "We’re experienced, trusted specialists. By working with industries we deeply understand, we confidently promise real, measurable, and sustainable results."}
-]
+
 
 const Different = () => {
+  const tHeader = useTranslations("HeaderOfDeffrent");
+  const header = tHeader('header')
+  const tDeffrentCards = useTranslations("DeffrentCards");
+  const cards =[
+    { title : tDeffrentCards("title_1"), description : tDeffrentCards("content_1")},
+    {title : tDeffrentCards("title_2"), description :tDeffrentCards("content_2")},
+    {title : tDeffrentCards("title_3") , description : tDeffrentCards("content_3")}
+  ]
   return (
     <section className="py-10 flex flex-col items-center container mx-auto w-full">
         <h2 className="text-3xl font-bold font-mono mb-8 text-center dark:text-gray-300 text-black">
-        OK... But What Makes Us Different?
+        {header}
         </h2>
         <div>
           <MeteorsDemo items={cards}/>
