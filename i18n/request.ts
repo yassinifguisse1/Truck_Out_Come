7977136a2fc3bb2516cdcf53834f091990cb8276
yassinifguisse1 <36793818+yassinifguisse1@users.noreply.github.com>
2024-10-25 23:17:44 +0200

    modift Package.json

--- i18n/request.ts
+++ i18n/request.ts
@@ -4,6 +4,7 @@ import {routing} from './routing';
 
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!routing.locales.includes(locale as any)) notFound();
 
   return {
