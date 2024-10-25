a8ad12323fe4a03fbf0bbc340b97feb784bb94f7
yassinifguisse1 <36793818+yassinifguisse1@users.noreply.github.com>
2024-10-26 00:44:59 +0200

    add THREE library

--- app/_components/canvas/StarsCanvas.tsx
+++ app/_components/canvas/StarsCanvas.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, Suspense, HTMLProps } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as THREE from 'three';
 import * as random from "maath/random/dist/maath-random.esm";
 
 // Define a Props interface if you expect to receive additional props
