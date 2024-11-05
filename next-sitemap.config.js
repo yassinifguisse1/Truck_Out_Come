dcd235e52cc46e4a148ce9e7483a38f21ac6c88a
yassinifguisse1 <36793818+yassinifguisse1@users.noreply.github.com>
2024-11-05 18:00:03 +0100

    final re

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://trackoutcome.agency', // Replace with your domain
+    generateRobotsTxt: true,           // (optional) Create a `robots.txt` file
+  };
+  
+  module.exports = config
